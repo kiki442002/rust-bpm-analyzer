7bd1bc79fae08413053bd643c878400b60c6cf0b
Killian <picot.killian@outlook.fr>
2026-01-12 03:28:37 +0100

    feat: ajout de la gestion des dépendances pour la prise en charge multi-architecture

--- src/core_bpm/mod.rs
+++ src/core_bpm/mod.rs
@@ -4,3 +4,6 @@ pub mod pid_audio;
 
 pub use analyzer::BpmAnalyzer;
 pub use audio::AudioCapture;
+
+#[cfg(all(any(target_arch = "aarch64", target_arch = "arm"), target_os = "linux"))]
+pub use pid_audio::AudioPID;
