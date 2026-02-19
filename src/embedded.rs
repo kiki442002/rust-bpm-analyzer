e4d471e3a7c805e6dc32ade71349b24d756e9743
Killian <picot.killian@outlook.fr>
2026-02-19 04:07:02 +0100

    ajoute un filtrage des messages réseau pour ignorer les niveaux d'énergie et affiche les autres messages reçus

--- src/embedded.rs
+++ src/embedded.rs
@@ -91,6 +91,9 @@ pub async fn run() -> Result<(), Box<dyn std::error::Error>> {
         // --- Poll Network Messages ---
         if let Some(net) = &network_manager {
             while let Ok(cmd) = net.try_recv() {
+                if !matches!(cmd, NetworkMessage::EnergyLevel(_)) {
+                    println!("Network Message Received: {:?}", cmd);
+                }
                 match cmd {
                     NetworkMessage::SetAutoGain(val) => {
                         println!("Network: SetAutoGain {}", val);
